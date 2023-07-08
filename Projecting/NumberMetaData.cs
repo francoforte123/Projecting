a6750bc02631e847edcc5507ce3dbda4e4e41244
rober <rober@DESKTOP-MN9AKPU>
2023-07-08 16:25:44 +0200

    Aggiungere i file di progetto.

---
+++ Projecting/NumberMetaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecting
+{
+    internal class NumberMetaData
+    {
+        public bool IsEven { get; set; }
+        public int NumberOfCharacters { get; set; }
+    }
+}
---
+++ Projecting/Program.cs
@@ -0,0 +1,20 @@
+﻿using Projecting;
+
+Random random = new Random();
+
+List<int> numbers = Enumerable.Range(0, 101)
+    .Select(_ => random.Next(0, 101))
+    .ToList();
+
+List<NumberMetaData> metadataList = numbers
+    .Select(num => new NumberMetaData
+    {
+        IsEven = num % 2 == 0,
+        NumberOfCharacters = num.ToString().Length
+    })
+    .ToList();
+
+foreach (NumberMetaData metadata in metadataList)
+{
+    Console.WriteLine("pari: " + metadata.IsEven + ", numero dei caratteri: " + metadata.NumberOfCharacters);
+}
